e742d579df5ac7b10f6537d0a56cc6570059cf20
inyare <23040145@alumno.utc.edu.mx>
2025-08-10 19:37:24 -0600

    movimientos en paginas de servicios

--- atom/citas.php
+++ atom/citas.php
@@ -6,6 +6,7 @@
     <title>Agendar Cita</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" href="image/ATOM LOGO.png" type="image/x-icon">
+    <link rel="stylesheet" href="fate.css">
     <style>
         body {
             font-family: Arial, sans-serif;
